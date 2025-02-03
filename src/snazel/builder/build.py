b4eeb1f56e522780fd1f0afb30f40c5f5ff02b1e
David Kristiansen <david@kristiansen.tech>
2025-02-03 22:23:34 +0000

    fix dependency graph

--- src/snazel/builder/build.py
+++ src/snazel/builder/build.py
@@ -9,12 +9,8 @@ logger = logging.getLogger(__name__)
 
 def build(rule_str: str):
     rules: dict[str, Rule] = dict()
-    root_rule = Rule(rule_str, workdir=cwd)
-    rules[str(root_rule)] = root_rule
-
-    dependency_stack: list[str] = root_rule._dependencies
+    dependency_stack: list[str] = [rule_str]
     dependency_graph: dict[str, list[str]] = dict()
-    dependency_graph[str(root_rule)] = root_rule.dependencies
 
     while len(dependency_stack) != 0:
         dependency = dependency_stack.pop()
@@ -22,11 +18,12 @@ def build(rule_str: str):
             continue
 
         rule = Rule(dependency)
-        dependency_stack.extend(rule._dependencies)
+        dependency_stack.extend(rule.dependencies)
         rules[str(rule)] = rule
         dependency_graph[str(rule)] = rule.dependencies
 
     ts = TopologicalSorter(dependency_graph)
+    print(dependency_graph)
     print(tuple(ts.static_order()))
 
 
